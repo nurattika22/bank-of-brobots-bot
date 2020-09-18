c6b3b09c0f8095637af7d42618364edc20c724fc
Andrew Yatsura <andrewyazura203@gmail.com>
2020-09-18 18:30:31 +0300

    Update README.md

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ load_config()
 localization = localization['en']
 
 bot = telebot.AsyncTeleBot(environ.get(
-    'TELEGRAM_API_TOKEN_DEV'), parse_mode='HTML')
+    'TELEGRAM_API_TOKEN'), parse_mode='HTML')
 
 
 @bot.message_handler(commands=['start'])
